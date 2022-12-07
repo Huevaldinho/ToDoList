e6bbb6bf3fe9fc48b1e293537a05121207a97e12
Huevaldinho <47650141+Huevaldinho@users.noreply.github.com>
2022-12-07 15:59:06 -0600

    delete react  default files and add some comments

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>This is the App component</h1>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
+//React imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//My imports
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {App} from "./App"
 
+//To manage the index.html document
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>
+  
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+  GitHub
+    Steps to upload the project to the github repository:
+      1) git add .
+      2) git commit -m "name of commit"
+      3) git remote add origin URLOfRepository
+      4) git push -u origin master
+
+   npm
+
+    Steps to start running the project
+      1) npm start
+*/
